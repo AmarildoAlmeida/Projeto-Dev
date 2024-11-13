270da16d4e5da67782430b2a0e3e8733b80967c3
AmarildoAlmeida <amarildo.almeida.s@gmail.com>
2024-11-13 03:56:53 -0400

    first commit

---
+++ src/app/models/Product.js
@@ -0,0 +1,18 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Product extends Model {
+  init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        price: Sequelize.INTEGER,
+        category: Sequelize.STRING,
+        path: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      },
+    );
+  }
+}
+export default Product;
