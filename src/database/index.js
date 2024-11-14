f330edfb5347fa25a8afc82875a5d38ec2f5654a
AmarildoAlmeida <amarildo.almeida.s@gmail.com>
2024-11-14 10:02:50 -0400

    Cria_Rela_Tab

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,7 @@ import User from '../app/models/User';
 import Product from '../app/models/Product';
 import Category from '../app/models/Category';
 
-const models = [User, Product,Category];
+const models = [User, Product, Category];
 
 class Database {
   constructor() {
@@ -14,7 +14,11 @@ class Database {
   }
   init() {
     this.connection = new Sequelize(configDatabase);
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      );
   }
 }
 
